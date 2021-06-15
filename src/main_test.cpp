ab01e5909723efab0e0e72809dd2a8a43eb26a35
Michael Thomas Wells <michaelthomaswells@gmail.com>
2021-06-15 03:07:34 +0000

    add unittests for opencv mat
    - add index.html form websocket test client
    
    Signed-off-by: Michael Thomas Wells <michaelthomaswells@gmail.com>

--- src/main_test.cpp
+++ src/main_test.cpp
@@ -1,6 +1,17 @@
 #include <gtest/gtest.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/hal/interface.h>
 
 TEST(OpenCV_Serialization, Serialize)
 {
-    ASSERT_EQ(true, false);
+  cv::Mat image;
+  uint16_t rows = 480;
+  uint16_t cols = 640;
+  image.create(rows, cols, CV_8U);
+  image.at<double>(0,0) = 1;
+  
+  ASSERT_EQ(image.at<double>(0,0), 1);
+  ASSERT_EQ(image.channels(), 1);
+  ASSERT_EQ(image.depth(), 0);
+  ASSERT_EQ(true, true);
 }
